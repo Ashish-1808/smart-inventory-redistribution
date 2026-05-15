868139bff242169357d46b0c2ba7a89ee3605a57
Ashish Walke <ashishwalke1808@gmail.com>
2026-05-15 12:36:01 +0530

    Log Cron Execution History

--- my-industry-project/backend/src/jobs/cron.job.js
+++ my-industry-project/backend/src/jobs/cron.job.js
@@ -4,9 +4,23 @@ import weatherService from "../modules/weather/weather.service.js";
 import redistributionService from "../modules/redistribution/redistribution.service.js";
 import { query } from "../config/database.js";
 
+//helper to log cron execution
+const logCronExecution = async (jobName, status, message) => {
+  try {
+    await query(
+      `INSERT INTO cron_logs(job_name,status,message)VALUES($1,$2,$3)`,
+      [jobName, status, message],
+    );
+  } catch (error) {
+    console.error("Log insertion failed", error.message);
+  }
+};
+
 //Forecast jobs(every 30 minutes)
 const startForecastJob = () => {
   cron.schedule("*/10 * * * * *", async () => {
+    const jobName = "WEATHER_FORECAST_JOB";
+    const start = Date.now();
     try {
       console.log("Running Weather + Forecast Job....");
 
@@ -21,8 +35,16 @@ const startForecastJob = () => {
         await forecastingService.generateForecast(warehouse.id);
       }
       console.log("Weather + Forecast Job Completed");
+
+      const time = Date.now() - start;
+      //log success
+      await logCronExecution(jobName, "SUCCESS", `Job completed in ${time}ms`);
     } catch (error) {
       console.log("Cron Forecast Job Failed", error.message);
+
+      const time = Date.now() - start;
+      //log failure
+      await logCronExecution(jobName, "FAILED", error.message);
     }
   });
 };
@@ -30,12 +52,21 @@ const startForecastJob = () => {
 //Redistribution Job
 const startRedistributionJob = () => {
   cron.schedule("*/15 * * * * *", async () => {
+    const jobName = "REDISTRIBUTION_JOB";
+    const start = Date.now();
     try {
       console.log("Running Redistribution Job...");
       await redistributionService.redistribution();
       console.log("Redistribution Job Completed...");
+
+      const time = Date.now() - start;
+      //log success
+      await logCronExecution(jobName, "SUCCESS", `Job completed in ${time}ms`);
     } catch (error) {
       console.error("Redistribution Job Failed");
+      const time = Date.now() - start;
+      //log failure
+      await logCronExecution(jobName, "FAILED", error.message);
     }
   });
 };
